ef19a085b2f154bee1605c05c9a954f074782d33
Tiberiu Frățilă <tiberiu.fratila@gmail.com>
2023-02-25 21:07:33 +0100

    Open setup page after install

--- src/background.js
+++ src/background.js
@@ -1,21 +1,4 @@
 'use strict';
 
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
-
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
-  }
-});
+// Open configuration window after installation
+chrome.runtime.onInstalled.addListener(({ reason }) => { if (reason === 'install') chrome.tabs.create({ url: 'https://sites.google.com/view/rug-authentication-assistant/#h.yn4h1kab0otd' }); });
